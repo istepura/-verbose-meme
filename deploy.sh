fa1dfb0430151bb7bcc0cf934598c4c5e9cb9c68
Igor Stepura <igor.stepura@gmail.com>
2019-10-12 13:44:26 -0400

    Add GKE deployment

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ docker push istepura/fibonator_worker:$GIT_SHA
 
 kubectl apply -f k8s
 
-kucbectl set image deployments/server-deployment server=istepura/fibonator_api:$GIT_SHA
-kucbectl set image deployments/client-deployment client=istepura/fibonator_client:$GIT_SHA
-kucbectl set image deployments/worker-deployment worker=istepura/fibonator_worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=istepura/fibonator_api:$GIT_SHA
+kubectl set image deployments/client-deployment client=istepura/fibonator_client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=istepura/fibonator_worker:$GIT_SHA
 
